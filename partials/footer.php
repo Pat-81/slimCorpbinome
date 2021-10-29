098373f3bdaeaa126155783003de82566fbf9f62
Pat <52626321+Pat-81@users.noreply.github.com>
2021-10-29 15:29:35 +0200

    Ajout du footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,9 +1,35 @@
-   </main>
-   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
-   <!--script-->
-   <script src="js/main.js"></script>
-   </body>
 
-   </html>
+</main>
+<footer class="d-flex text-center p-2 text-white  justify-content-around bg-success">
+  <div class="col-sm-10 col-md-8 col-lg-8 col-xl-3 mx-1 ">
+    <h2 class="text-center pb-2 h4">Newsletter</h2>
+    <form>
+      <div class="form-group">
+        <label for="formGroupExampleInput">Recevez notre newsletter</label>
+        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Entre votre adresse email">
+      </div>
+  </div>
+
+  <div class="col-sm-10 col-md-8 col-lg-8 col-xl-3">
+    <h2 class="text-center h4">Nos coordonnées</h2>        
+          <p class="pt-3"><i class="fas fa-map-marked-alt py-1">&nbsp 1 rue du test -&nbsp  00000 TestCity</i><br><i class="fas fa-phone py-1">&nbsp 00-01-02-03-04</i></p>
+  </div>
+
+  <div class="col-sm-10 col-md-8 col-lg-8 col-xl-3 ">
+    <h2 class="text-center pb-2 h4">Suivez- nous !</h2>
+      <a href="#"><i class="social fab fa-facebook pt-3 px-1">&nbsp Facebook</i></a>
+      <a href="#"><i class="social fab fa-twitter pt-3 px-1">&nbsp Twitter</i></a>
+      <a href="#"><i class="social fab fa-tiktok pt-3 px-1">&nbsp Tiktok</i></a>
+      <a href="#"><i class="social fab fa-instagram pt-3 px-1">&nbsp Instagram</i></a>
+  </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>    <!--script-->
+<script src="js/script.js"></script>
+<script src="js/projets.js"></script>
+<script src="js/form.js"></script>
+</body>
+
+</html>
