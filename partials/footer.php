e3a5eccc28517a37fe9cff3c7be995367a25aa00
Pat <52626321+Pat-81@users.noreply.github.com>
2021-10-31 17:54:46 +0100

    Réorganisation projets.js

--- partials/footer.php
+++ partials/footer.php
@@ -39,8 +39,8 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 <!--script-->
-<script src="https://maps.google.com/maps/api/js?key=AIzaSyC2nqyjJXAQUiDstx3Km8vP4GBGOZfy__A" type="text/javascript"></script>
-<script async type="text/javascript">
+<!-- <script src="https://maps.google.com/maps/api/js?key=AIzaSyC2nqyjJXAQUiDstx3Km8vP4GBGOZfy__A" type="text/javascript"></script> -->
+<!-- <script async type="text/javascript">
   // On initialise la latitude et la longitude de Paris (centre de la carte)
   var lat = 48.852969;
   var lon = 2.349903;
@@ -75,10 +75,9 @@
     // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
     initMap();
   };
-</script>
+</script> -->
 <script src="js/main.js"></script>
-<script src="js/projets.js"></script>
-<script src="js/formulaire.js"></script>
+<script src="js/projet2.js"></script>
 
 </body>
 
--- projets.php
+++ projets.php
@@ -1,15 +1,20 @@
 <?php require_once 'partials/head.php' ?>
 
-    <h1 id="titre" class="text-center py-3">SLIME CORP</h1>
-    <div class=" row no-gutters justify-content-around p-5" id="projets">
-        <div class="partner col-lg-6 pt-2 text-center">
-            <h2 class="text-center pt-5 pb-5">Nos partenaires</h2>
-            <div id="partenaires">
-            </div>
+<h1 id="titre" class="text-center py-3">SLIME CORP</h1>
+<div class="row no-gutters justify-content-around p-5" id="projets">
+    <div class="partner  col-lg-12 pt-2 text-center">
+        <h2 class="text-center pt-5 pb-5">Nos partenaires</h2>
+        <div id="partenaires">
+            <!-- <div class="border d-inline-block rounded align-items-center">
+                <img id="logo" src="img/B9717039560Z.1_20180925145002_000+GISC3O7NA.1-0.jpg" alt="">
+                <input type="checkbox" name="" id="">
+                <a href="#">Partenaire</a>
+            </div> -->
         </div>
-
-        <div class="partner col-lg-6 pt-2 pb-5">
-            <h2 class="text-center pt-5">Soutenez-nous</h2>
+    </div>
+    <div class="partner don-partner col-lg-12  pt-2 pb-5 text-center">
+        <form class="mx-auto">
+            <h2 class=" text-center ">Soutenez-nous</h2>
             <div class="input-group pt-5 ">
                 <div class="input-group-prepend mx-auto pt-3 pb-5">
                     <label class="input-group-text don" for="inputGroupSelect01">Votre don</label>
@@ -23,19 +28,17 @@
                     </select>
                 </div>
             </div>
-            <form action="">
-                <div class="form-check text-center pt-3">
-                    <input id="don-checkbox" type="checkbox" class="form-check-input" id="exampleCheck1 ">
-                    <label class="form-check-label" for="exampleCheck1">Veuillez accepter pour valider votre don</label>
-                </div>
-                <button id="don" class="btn btn-outline-light d-block mx-auto mt-3 disabled">Valider</button>
-            </form>
-        </div>
+            <div class="form-check text-center pt-3">
+                <input id="don-checkbox" type="checkbox" class="form-check-input" id="exampleCheck1 ">
+                <label class="form-check-label" for="exampleCheck1">Veuillez accepter pour valider votre don</label>
+            </div>
+            <button id="don" class="btn btn-outline-light d-block mx-auto mt-3 disabled">Valider</button>
+        </form>
     </div>
-    <button id="ajout" class="btn btn-lg d-block mx-auto mt-3">Ajouter un partenaire</button>
-    <div id="formulaire" class="col-lg-4 mx-auto d-flex flex-column-reverse justify-content-center">
-     
+</div>
+<button id="ajout" class="btn btn-lg d-block mx-auto mt-3">Ajouter un partenaire</button>
+<div id="formulaire" class="col-lg-4 mx-auto d-flex flex-column-reverse justify-content-center">
     <button id="btnAddPartner" class="btn btn-outline-light  mx-auto mt-3  hide disabled" type="submit">Valider</button>
-    </div>
+</div>
 
-    <?php require_once 'partials/footer.php' ?>
+<?php require_once 'partials/footer.php' ?>
